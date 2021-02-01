a27c60221a1133836e84ce73bff89dbf20b70f68
Dena Sabha <denasabha@gmail.com>
2021-02-01 10:57:38 -0800

    adding messages to a list

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
+
+import { useState } from 'react';
 import './App.css';
+import TextInput from './TextInput';
 
 function App() {
+  const [messages,setMessages] = useState([{text:'hello'}])
+
+  console.log(messages)
+
   return <div className="App">
+ 
     <header className="header">
-      <div className="logo"/>
+      <div className="logo" />
       CHATTER BOX
     </header>
+
+    <main className="messages">
+      {messages.map((m,i)=> {
+        return <div key={i} className="message-row">
+          <div className="message">
+            {m.text}
+          </div>
+        </div>
+      })}
+    </main>
+
+    <TextInput 
+      send={(t)=> setMessages( [...messages, {text:t}] )}
+    />
+
   </div>
 }
+
 export default App;
